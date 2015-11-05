80ae97b684f8f33810594327731badbac9c2a62c
hassanselim0 <hassanselim0@hotmail.com>
2015-11-05 23:12:51 +0200

    - Fixed the "Update Server" feature in the Admin page

--- MinecraftWebToolkit/Controllers/AdminController.cs
+++ MinecraftWebToolkit/Controllers/AdminController.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Cache;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 using System.Web.Security;
 
@@ -69,28 +72,58 @@ namespace MinecraftWebToolkit.Controllers
 
         public ActionResult UpdateServer()
         {
-			// https://s3.amazonaws.com/Minecraft.Download/versions/versions.json
-			// https://s3.amazonaws.com/Minecraft.Download/versions/" + Ver + "/minecraft_server." + Ver + ".jar
-			
-            var client = new System.Net.WebClient();
+            // http://assets.minecraft.net/ <- This is an XML file
+            // http://assets.minecraft.net/V_E_R/minecraft_server.jar
+            // Old Stuff, from beta 1.8 pre till 1.5.2, and from 11w47 till 13w12 snapshots
 
-            HttpContext.Application["UpdateProgress"] = "0%";
+            // https://s3.amazonaws.com/Minecraft.Download/versions/versions.json
+            // https://s3.amazonaws.com/Minecraft.Download/versions/V.E.R/minecraft_server.V.E.R.jar
+            // Minimum Available Server Version: 1.2.5
+
+            var client = new System.Net.WebClient();
+            client.CachePolicy = new RequestCachePolicy(RequestCacheLevel.CacheIfAvailable);
+
+            HttpContext.Application["UpdateProgress"] = "Getting Latest Version...";
+
+            // Get Latest Version
+            var jObj = JObject.Parse(client.DownloadString(
+                "https://s3.amazonaws.com/Minecraft.Download/versions/versions.json"));
+            var ver = jObj["latest"]["release"].Value<string>();
+
+            var jarFile = "minecraft_server." + ver + ".jar";
+            var jarUri = "https://s3.amazonaws.com/Minecraft.Download/versions/" + ver + "/" + jarFile;
+
+            var config = WebConfigurationManager.OpenWebConfiguration("~");
 
             client.DownloadProgressChanged += (o, e) =>
-            {
                 HttpContext.Application["UpdateProgress"] = e.ProgressPercentage + "%";
-            };
 
             client.DownloadFileCompleted += (o, e) =>
             {
+                if (e.Error != null)
+                {
+                    HttpContext.Application["UpdateProgress"] = "Error: " + e.Error;
+                    return;
+                }
+
                 HttpContext.Application["UpdateProgress"] = "Completed";
+
+                config.AppSettings.Settings["McJarFile"].Value = jarFile;
+                config.Save();
             };
 
-            System.Threading.Tasks.Task.Run(() =>
+            HttpContext.Application["UpdateProgress"] = "0%";
+            System.Threading.Tasks.Task.Run(() => // Workaround to allow Async call
             {
-                client.DownloadFile(new Uri("http://www.minecraft.net/download/minecraft_server.jar"),
-                    System.Configuration.ConfigurationManager.AppSettings["McServerPath"] + "minecraft_server.jar");
-                HttpContext.Application["UpdateProgress"] = "Completed";
+                try
+                {
+                    client.DownloadFileAsync(new Uri(jarUri),
+                        config.AppSettings.Settings["McServerPath"].Value + jarFile);
+                }
+                catch (Exception ex)
+                {
+                    HttpContext.Application["UpdateProgress"] = "Error: " + ex;
+                }
             });
 
             return Content("OK " + DateTime.Now.Ticks);
