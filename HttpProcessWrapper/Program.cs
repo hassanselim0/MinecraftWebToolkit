df0cfb0572d09e39bacca93e7125137a7c2d45cb
hassanselim0 <hassanselim0@hotmail.com>
2015-11-17 01:13:29 +0200

    - Added some code in HttpProcessWrapper/Program.cs to make it easy to test routes right from the console

--- HttpProcessWrapper/Program.cs
+++ HttpProcessWrapper/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using Nancy;
 using Nancy.Hosting.Self;
+using System;
 
 namespace HttpProcessWrapper
 {
@@ -7,19 +8,34 @@ namespace HttpProcessWrapper
     {
         static void Main(string[] args)
         {
-            var uri = new Uri("http://localhost:25564");
-            
+            var baseUri = new Uri("http://localhost:25564");
+
             var config = new HostConfiguration();
             //config.UrlReservations.CreateAutomatically = true;
             config.RewriteLocalhost = false;
 
-            using (var host = new NancyHost(config, uri))
+            var bootstrapper = new Bootstrapper();
+            using (var host = new NancyHost(bootstrapper, config, baseUri))
             {
                 host.Start();
 
-                Console.WriteLine("Your application is running on " + uri);
-                Console.WriteLine("Press [Enter] to close the host.");
-                while (Console.ReadKey().Key != ConsoleKey.Enter) ;
+                Console.WriteLine("Your application is running on: " + baseUri);
+                Console.WriteLine("You can type relative URIs to test routes, or enter an empty line to exit");
+
+                var engine = bootstrapper.GetEngine();
+
+                while (true)
+                {
+                    var line = Console.ReadLine();
+                    if (line == "") break;
+
+                    var req = new Request("GET", new Url(baseUri + line));
+                    var res = engine.HandleRequest(req).Response;
+
+                    Console.WriteLine("Response Code: " + res.StatusCode);
+                    res.Contents(Console.OpenStandardOutput());
+                    Console.WriteLine();
+                }
 
                 ProcModule.KillAllProcs();
             }
