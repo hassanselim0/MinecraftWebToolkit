7cd564db6f2c88086e5d6c6366f45e55cb1e466c
hassanselim0 <hassanselim0@hotmail.com>
2015-11-17 22:14:02 +0200

    - Prevented HttpProcessWrapper from trying to AutoRegister MCWTK's assembly and thus preventing it from getting rebuilt (dev scenario)

--- HttpProcessWrapper/Bootstrapper.cs
+++ HttpProcessWrapper/Bootstrapper.cs
@@ -1,4 +1,8 @@
 ﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace HttpProcessWrapper
 {
@@ -7,5 +11,17 @@ namespace HttpProcessWrapper
         // The bootstrapper enables you to reconfigure the composition of the framework,
         // by overriding the various methods and properties.
         // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+
+        protected override IEnumerable<Func<Assembly, bool>> AutoRegisterIgnoredAssemblies
+        {
+            get
+            {
+                return base.AutoRegisterIgnoredAssemblies.Union(new Func<Assembly, bool>[]
+                {
+                    asm => asm.FullName.Contains("MinecraftWebToolkit"),
+                    asm => asm.FullName.Contains("MCWTK"),
+                });
+            }
+        }
     }
 }
