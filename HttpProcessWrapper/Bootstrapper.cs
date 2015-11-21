f43475aae2d9a653b472a28e3e7045cd6b03e4cc
hassanselim0 <hassanselim0@hotmail.com>
2015-11-21 16:41:16 +0200

    - Revert 7cd564db6f2c88086e5d6c6366f45e55cb1e466c because that didn't really work

--- HttpProcessWrapper/Bootstrapper.cs
+++ HttpProcessWrapper/Bootstrapper.cs
@@ -1,8 +1,4 @@
 ﻿using Nancy;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 
 namespace HttpProcessWrapper
 {
@@ -11,17 +7,5 @@ namespace HttpProcessWrapper
         // The bootstrapper enables you to reconfigure the composition of the framework,
         // by overriding the various methods and properties.
         // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
-
-        protected override IEnumerable<Func<Assembly, bool>> AutoRegisterIgnoredAssemblies
-        {
-            get
-            {
-                return base.AutoRegisterIgnoredAssemblies.Union(new Func<Assembly, bool>[]
-                {
-                    asm => asm.FullName.Contains("MinecraftWebToolkit"),
-                    asm => asm.FullName.Contains("MCWTK"),
-                });
-            }
-        }
     }
 }
